98d60858883c6edc1905805efc332c29814bef36
dreampursuer95 <36196473+dreampursuer95@users.noreply.github.com>
2021-10-07 21:14:21 +0800

    Refactor to move constants out

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+import utils.constants as constants
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,27 +9,21 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    dia_response = "Can it be, that you’re talking about μ’s? How dare you mistaken their name?! Hm? In the school " \
-                   "idol world, μ’s are legendary. They’re the holy ground, holy scripture, the origin of life " \
-                   "equivalent to the universe. And you mistaken their name?! Absolutely ridiculous. "
-    if "u's" in message.content:
-        await message.channel.send(dia_response)
+    if constants.muse_trigger in message.content:
+        await message.channel.send(constants.dia_response)
 
-    dragon_response = "Dragons are gay"
-    if message.content == "dragon":
-        await message.channel.send(dragon_response)
-
-    egypt_response = "I lub Egypt"
-    if message.content == "Egypt is the best":
-        await message.channel.send(egypt_response)
+    if constants.warn_trigger in message.content:
+        await message.channel.send(constants.nauseated_face_response)
 
 client.run(TOKEN)
---
+++ utils/constants.py
@@ -0,0 +1,9 @@
+# Trigger Words
+muse_trigger = "u's"
+warn_trigger = "!warn"
+
+# Responses
+dia_response = "Can it be, that you’re talking about μ’s? How dare you mistaken their name?! Hm? In the school " \
+               "idol world, μ’s are legendary. They’re the holy ground, holy scripture, the origin of life " \
+               "equivalent to the universe. And you mistaken their name?! Absolutely ridiculous. "
+nauseated_face_response = ":nauseated_face"
